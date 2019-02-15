b8dcbad8d70465e14568ede2a6851227a56e9ff9
plfisher <47302771+plfisher@users.noreply.github.com>
2019-02-15 17:28:45 -0500

    Update art_collector.c
    sort linked list

--- art_collector.c
+++ art_collector.c
@@ -21,10 +21,7 @@ typedef struct  {
 	int price;
 }art_piece;
 
-
-
 art_piece *createArtPiece(int id, char *t, char *artn, char *artistn, int price){
-
 art_piece *p= malloc(sizeof(art_piece));
 p->id = id;
 strcpy(p->type, t);
@@ -33,12 +30,51 @@ strcpy(p->artistname, artistn);
 p->price=price;
 return p;
 }
+
 typedef struct {
-	art_piece *p;
-	struct node *next;
-	struct node *prev;
+	art_piece *data;
+	node *next;
+	node *prev;
 
 }node;
+
+void insertAtBegin(node **start_ref, art_piece data)
+{
+	node *ptr1 = new node;
+	ptr1->data=data;
+	ptr1->next = *start_ref;
+	if(*start_ref != NULL)
+		(*start_ref)->prev=ptr1;
+	*start_ref+ptr1;
+}
+
+void sort(node *start)
+{
+	int swapper, i;
+	node *ptr1;
+	node *lptr = NULL;
+	
+	if(start==NULL)
+		return;
+	do
+	{
+		swapped=0;
+		ptr1=start;
+		while(ptr1->next != lptr)
+		{
+			if(ptr1->data->id > ptr1->next->data->id)
+			{
+				swap(ptr1->data, ptr1->next->data);
+				swapped=1;
+			}
+			ptr1=ptr1->next;
+		}
+		lptr = ptr1;
+	}
+	while (swapped);
+}
+
+
 
 
 
