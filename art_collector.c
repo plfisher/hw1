a6bb3d30dfc7c4d5781a9a179ade303c6cedd82b
plfisher <47302771+plfisher@users.noreply.github.com>
2019-02-15 16:48:33 -0500

    Update art_collector.c

--- art_collector.c
+++ art_collector.c
@@ -6,10 +6,10 @@
 char *strcpy(char *dest, const char *src)
 {
 	unsigned i;
-	for(i=0; (&(src+i))!='\0'; i++)
-		&(dest+i)=&(src+i);
+	for(i=0; (*(src+i))!='\0'; i++)
+		*(dest+i)=*(src+i);
 	
-	&(dest+i)='\0';
+	*(dest+i)='\0';
 	return dest;
 }
 
